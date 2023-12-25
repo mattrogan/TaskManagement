c371755bd70b84a57e81793dbf965bd7b01ec710
Matt Rogan <matt.rogan@cubic-interactive.com>
2023-12-25 16:11:46 +0000

    Add ctx to controller and seed initial values for tasks

--- Server/Controllers/TodoItemController.cs
+++ Server/Controllers/TodoItemController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TaskManagement.Shared.Models;
 using System.Linq;
+using TaskManagement.Server.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace TaskManagement.Server.Controllers
 {
@@ -9,19 +11,31 @@ namespace TaskManagement.Server.Controllers
     public class TodoItemController : ControllerBase
     {
         private readonly ILogger<TodoItemController> logger;
+        private readonly TaskContext ctx;
 
-        private static readonly IEnumerable<TodoItem> Tasks = new[] {
-            new TodoItem("Foo", "Bar", DateTime.Now),
-            new TodoItem("Lorem Ipsum", "Dolor Sit", DateTime.Now),
-        };
-
-        public TodoItemController(ILogger<TodoItemController> logger)
+        public TodoItemController(ILogger<TodoItemController> logger, TaskContext ctx)
         {
             this.logger = logger;
+            this.ctx = ctx;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetTasksAsync()
-            => Ok(Tasks.ToList());
+        {
+            var todoItems = await ctx.TodoItems.ToListAsync();
+            return Ok(todoItems);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTaskAsync(int id)
+        {
+            var todoItem = await ctx.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound(id);
+            }
+
+            return Ok(todoItem);
+        }
     }
 }
---
+++ Server/Migrations/20231225160835_SeedInitialTasks.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using TaskManagement.Server.SeedData;
+
+#nullable disable
+
+namespace TaskManagement.Server.Migrations
+{
+    public partial class SeedInitialTasks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            foreach (var task in SeedData_InitialTasks.InitialTasks)
+            {
+                migrationBuilder.InsertData(
+                    table: "TodoItems",
+                    columns: new[] { "Id", "Title", "Description", "DueDate" },
+                    values: new object[] { task.Id, task.Title, task.Description, task.DueDate });
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            foreach (var task in SeedData_InitialTasks.InitialTasks)
+            {
+                migrationBuilder.DeleteData(
+                    table: "TodoItems",
+                    keyColumn: "Id",
+                    keyValue: task.Id);
+            }
+        }
+    }
+}
