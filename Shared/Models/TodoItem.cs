051e572a9aeec76b2960228730db89bd0c6177c7
Matt Rogan <matt.rogan@cubic-interactive.com>
2023-12-25 12:50:08 +0000

    Add unit tests for todo item class, add constructors and overloaded equals

--- Shared/Models/TodoItem.cs
+++ Shared/Models/TodoItem.cs
@@ -6,6 +6,18 @@ namespace TaskManagement.Shared.Models
     [DataContract]
     public class TodoItem
     {
+        public TodoItem()
+        {
+        }
+
+        public TodoItem(string title, string desc, DateTime dueDate)
+            : this()
+        {
+            Title = title;
+            Description = desc;
+            DueDate = dueDate;
+        }
+
         [DataMember(Name = "id")]
         [Required]
         public int Id { get; set; }
@@ -21,5 +33,24 @@ namespace TaskManagement.Shared.Models
         [DataMember(Name = "due")]
         [Required]
         public DateTime DueDate { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            TodoItem? other = obj as TodoItem;
+            if (other is null)
+            {
+                return false;
+            }
+
+            return this.Id == other.Id
+                && this.Title == other.Title
+                && this.Description == other.Description
+                && this.DueDate == other.DueDate;
+        }
     }
 }
