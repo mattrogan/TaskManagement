b2d1bf3919c272fb9992802e98352b0d99ac8fa8
Matt Rogan <matt.rogan@cubic-interactive.com>
2023-12-25 14:00:58 +0000

    Add TodoItemController with basic GET method

---
+++ Server/Controllers/TodoItemController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagement.Shared.Models;
+using System.Linq;
+
+namespace TaskManagement.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]/")]
+    public class TodoItemController : ControllerBase
+    {
+        private readonly ILogger<TodoItemController> logger;
+
+        private static readonly IEnumerable<TodoItem> Tasks = new[] {
+            new TodoItem("Foo", "Bar", DateTime.Now),
+            new TodoItem("Lorem Ipsum", "Dolor Sit", DateTime.Now),
+        };
+
+        public TodoItemController(ILogger<TodoItemController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTasksAsync()
+            => Ok(Tasks.ToList());
+    }
+}
