0de99e14482fe2f145b86c55e79a36e4a1595cee
Matt Rogan <matt.rogan@cubic-interactive.com>
2023-12-26 15:39:37 +0000

    Refactor database setup in context file

--- Server/Data/TaskContext.cs
+++ Server/Data/TaskContext.cs
@@ -5,6 +5,13 @@ namespace TaskManagement.Server.Data
 {
     public class TaskContext : DbContext
     {
+        private readonly IConfiguration cfg;
+
+        public TaskContext(IConfiguration cfg)
+        {
+            this.cfg = cfg;
+        }
+
         public DbSet<TodoItem> TodoItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -13,7 +20,16 @@ namespace TaskManagement.Server.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite("Data Source = TaskManagement.db");
+            string dbName = cfg["DbName"];
+            // Construct the full connection string
+            string connectionString = string.Format(cfg.GetConnectionString("DefaultConnection"), GetDatabasePath(dbName));
+            optionsBuilder.UseSqlite(connectionString);
+        }
+
+        private string GetDatabasePath(string dbName)
+        {
+            string specialFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            return Path.Combine(specialFolder, $"{dbName}.db");
         }
     }
 }
